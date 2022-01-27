d263dbb1e598db294222b37c0c678a1c5a72fa06
PM <princemakadiya002@gmail.com>
2022-01-27 17:20:36 +0530

    Add project files.

---
+++ Tatvasoft_Project/Controllers/HelperlandController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tatvasoft_Project.Controllers
+{
+    public class HelperlandController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Prices()
+        {
+            return View();
+        }
+
+        public ViewResult Contact()
+        {
+            return View();
+        }
+
+        public ViewResult FAQ()
+        {
+            return View();
+        }
+
+        public ViewResult About()
+        {
+            return View();
+        }
+        public ViewResult Become_helper()
+        {
+            return View();
+        }
+        public ViewResult New_user()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
